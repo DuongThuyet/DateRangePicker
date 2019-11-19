256468de17921f64ca950333cc405bd49fb35065
leducanh <demo.eaglecs@gmail.com>
2019-11-19 11:01:29 +0700

    Update UI

--- app/src/main/java/com/example/daterangepick/MainActivity.kt
+++ app/src/main/java/com/example/daterangepick/MainActivity.kt
@@ -26,7 +26,12 @@ class MainActivity : AppCompatActivity() {
 
         calendar.deactivateDates(list)
         val arrayList = mutableListOf<Date>()
+        val lstHighlightedDates = mutableListOf<Date>()
+        val currentCal = Calendar.getInstance()
+        val currentCalStr = SimpleDateFormat("dd-MM-yyyy").format(currentCal.time)
+
         try {
+
             val dateformat = SimpleDateFormat("dd-MM-yyyy")
             val strdate = "22-2-2018"
             val strdate2 = "26-2-2018"
@@ -38,10 +43,30 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
-        calendar.init(lastYear.getTime(), nextYear.getTime(), SimpleDateFormat("MMMM, YYYY", Locale.getDefault())) //
+        val instance = Calendar.getInstance()
+        instance.add(Calendar.DATE, -10)
+        val startCal = instance.clone() as Calendar
+        var startCalStr = SimpleDateFormat("dd-MM-yyyy").format(startCal.time)
+        while (startCalStr != currentCalStr) {
+            lstHighlightedDates.add(startCal.time)
+            startCal.add(Calendar.DATE, 1)
+            startCalStr = SimpleDateFormat("dd-MM-yyyy").format(startCal.time)
+        }
+
+        val minStartDate = Calendar.getInstance()
+        minStartDate.add(Calendar.DATE, 2)
+
+        val minEndDate = Calendar.getInstance()
+        minEndDate.add(Calendar.DATE, 5)
+
+        calendar.init(
+            minStartDate.time,
+            nextYear.time,
+            SimpleDateFormat("MMMM, YYYY", Locale.getDefault())
+        ) //
             .inMode(CalendarPickerView.SelectionMode.RANGE) //
-            .withSelectedDate(Date())
-            .withDeactivateDates(list)
-            .withHighlightedDates(arrayList)
+            .withSelectedDate(minStartDate.time)
+//            .withDeactivateDates(list)
+//            .withHighlightedDates(lstHighlightedDates)
     }
 }
--- rangedatepicker/src/main/java/com/savvi/rangedatepicker/MonthDescriptor.java
+++ rangedatepicker/src/main/java/com/savvi/rangedatepicker/MonthDescriptor.java
@@ -4,47 +4,49 @@ package com.savvi.rangedatepicker;
 import java.util.Date;
 
 class MonthDescriptor {
-  private final int month;
-  private final int year;
-  private final Date date;
-  private String label;
+    private final int month;
+    private final int year;
+    private final Date date;
+    private String label;
 
-  public MonthDescriptor(int month, int year, Date date, String label) {
-    this.month = month;
-    this.year = year;
-    this.date = date;
-    this.label = label;
-  }
+    public MonthDescriptor(int month, int year, Date date, String label) {
+        this.month = month;
+        this.year = year;
+        this.date = date;
+        this.label = label;
+    }
 
-  public int getMonth() {
-    return month;
-  }
+    public int getMonth() {
+        return month;
+    }
 
-  public int getYear() {
-    return year;
-  }
+    public int getYear() {
+        return year;
+    }
 
-  public Date getDate() {
-    return date;
-  }
+    public Date getDate() {
+        return date;
+    }
 
-  public String getLabel() {
-    return label;
-  }
+    public String getLabel() {
+        return label;
+    }
 
-  void setLabel(String label) {
-    this.label = label;
-  }
 
-  @Override public String toString() {
-    return "MonthDescriptor{"
-        + "label='"
-        + label
-        + '\''
-        + ", month="
-        + month
-        + ", year="
-        + year
-        + '}';
-  }
+    void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "MonthDescriptor{"
+                + "label='"
+                + label
+                + '\''
+                + ", month="
+                + month
+                + ", year="
+                + year
+                + '}';
+    }
 }
